164c2c2fe6adcd57e9f33d7752da8c79714025a3
priyanka-0122 <priyankasinha.2218@gmail.com>
2024-08-20 15:15:25 +0530

    Adding the concept of accessing characters and strings

---
+++ 20_char_str_literals/tests/input22.c
@@ -0,0 +1,13 @@
+void main()
+{
+        int a[3];
+        int b , c , d ,e , f;
+        a[1] = 200;
+        a[0] = 100;
+
+        b = a[0];
+        c = a[1];
+        printint(b);
+        printint(c);
+
+}
---
+++ 20_char_str_literals/tests/input24.c
@@ -0,0 +1,21 @@
+void main()
+{
+        int a [4];
+        int b , c , d ,e , f;
+        a[3] = 300;
+        a[2] = 200;
+        a[1] = 100;
+
+        b = a[1];
+        printint(b);
+
+        c = a[2];
+        printint(c);
+
+        d = a[3];
+        printint(d);
+
+        int g;
+        g = 18;
+        printint(g);
+}
